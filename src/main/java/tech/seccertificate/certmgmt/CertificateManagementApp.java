606070099a299dc6c5c2e56105dd56e830188b7f
Ivan Irakoze <iam.irakoze.io@gmail.com>
2025-11-28 18:17:21 +0200

    Inception

---
+++ src/main/java/tech/seccertificate/certmgmt/CertificateManagementApp.java
@@ -0,0 +1,13 @@
+package tech.seccertificate.certmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateManagementApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertificateManagementApp.class, args);
+    }
+
+}
