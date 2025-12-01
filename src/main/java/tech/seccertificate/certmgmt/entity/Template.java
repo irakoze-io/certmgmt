34f00d5415bc7e37753e4b640f82929fa64d63f6
Ivan Irakoze <iam.irakoze.io@gmail.com>
2025-12-01 19:27:36 +0200

    Organizing imports around the template table definition and updates

--- src/main/java/tech/seccertificate/certmgmt/entity/Template.java
+++ src/main/java/tech/seccertificate/certmgmt/entity/Template.java
@@ -1,6 +1,20 @@
 package tech.seccertificate.certmgmt.entity;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
