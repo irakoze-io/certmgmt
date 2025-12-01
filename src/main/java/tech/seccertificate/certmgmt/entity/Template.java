677eef1a01aac1ee1ca83ddaa9b113385ba93620
Ivan Irakoze <iam.irakoze.io@gmail.com>
2025-12-01 19:28:27 +0200

    Remove unused imports

--- src/main/java/tech/seccertificate/certmgmt/entity/Template.java
+++ src/main/java/tech/seccertificate/certmgmt/entity/Template.java
@@ -19,13 +19,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 @Data
 @Entity
