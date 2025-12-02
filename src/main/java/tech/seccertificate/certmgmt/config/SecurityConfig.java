6ca51768bfdaf914ec3fc926c9edc2d29f28298c
Ivan Irakoze <iam.irakoze.io@gmail.com>
2025-12-02 19:52:33 +0200

    Add OpenAPI and Spring Security configurations.
    This commit introduces OpenAPI configuration using SpringDoc with Scalar UI and basic security configuration via Spring Security. The changes include new dependencies, updated .gitignore, additional application properties, and relevant controllers, templates, and configurations for API documentation and security.

---
+++ src/main/java/tech/seccertificate/certmgmt/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package tech.seccertificate.certmgmt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .build();
+    }
+}
