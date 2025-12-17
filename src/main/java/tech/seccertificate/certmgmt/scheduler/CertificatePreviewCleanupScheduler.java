bd270b69ac49ad7de9b4d08c080a58f444b5dc65
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2025-12-17 23:51:46 +0200

    Relevant imports

--- src/main/java/tech/seccertificate/certmgmt/scheduler/CertificatePreviewCleanupScheduler.java
+++ src/main/java/tech/seccertificate/certmgmt/scheduler/CertificatePreviewCleanupScheduler.java
@@ -2,6 +2,7 @@ package tech.seccertificate.certmgmt.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import tech.seccertificate.certmgmt.service.CertificateService;
